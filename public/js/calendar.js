bfa25b0eb14d3e48605a4a1186437159dc6fae32
Hannali Patidar <hannalip@gmail.com>
2020-03-11 18:46:28 -0400

    push

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -3,7 +3,7 @@ const { DateTime } = require("luxon");
 // const dateFormat = "YYYY-MM-dd";
 const now = DateTime.local();
 
-//1)what's days of week of 1st of month?
+//1)what's day of week of 1st of month?
 const firstOfMonthDayOfWeek = now.startOf("month").weekday;
 console.log("Frist of Month Day of Week", firstOfMonthDayOfWeek);
 //how many days in this month?
