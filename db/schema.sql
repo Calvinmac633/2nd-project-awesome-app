9c4d409686068721f0db41c527e2d77e25e56272
Calvin Macintosh <calvinmac633@gmail.com>
2020-03-11 19:16:55 -0400

    seeds in the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,7 @@
 
 CREATE DATABASE calendar_tasks;
 USE calendar_tasks;
+
+INSERT INTO tasks(date, note, createdAt, updatedAt)
+VALUES ("03/16/2020", "This is the note for note 1", CURRENT_TIME(), CURRENT_TIME()),
+	("03/23/2020", "This is the note for note 222222", CURRENT_TIME(), CURRENT_TIME());
