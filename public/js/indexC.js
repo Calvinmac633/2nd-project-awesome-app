039a45333749b22c7aca723f4dc2aa2623cf9bc5
Hannali Patidar <hannalip@gmail.com>
2020-03-12 19:24:01 -0400

    button

--- public/js/indexC.js
+++ public/js/indexC.js
@@ -10,6 +10,15 @@
 $(document).ready(function() {
   $(document).on("click", ".day", function(e) {
     e.preventDefault();
+    console.log("Button clicked");
+    var date = $(this).attr("id");
+    console.log(date);
+    $.ajax(`/api/task/${date}`, {
+      type: "GET",
+      data: date
+    }).then(function() {
+      console.log(date);
+    });
     console.log($(this).attr("id"));
   });
 });
